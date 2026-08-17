5070a10643baef4809ddf74e052b5c77cbeaadaa
kasra <kasraam385@gmail.com>
2026-08-17 22:08:03 +0330

    add my work exp project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import HeroSection from "@/components/landing/hero-section";
 import LilNavigator from "@/components/navigation/lil-navigator";
 import GitHubContainer from "@/components/landing/github/github-container";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -25,15 +27,36 @@ export default function Home() {
                 <span className="bg-primary font-bold text-secondary relative -left-5 w-10 h-10 flex items-center justify-center">
                   1
                 </span>
-                <span className="text-xl font-semibold">
-                  Online super market
-                </span>
+                <div className="flex items-center gap-2 relative -left-1">
+                  <span className="text-xl font-semibold">
+                    Online super market
+                  </span>
+                  <span>-</span>
+                  <Button
+                    asChild
+                    variant={"link"}
+                    className="opacity-60 duration-100 hover:opacity-100 p-0 underline"
+                    size={"sm"}
+                  >
+                    <Link
+                      href={`https://mohosseinimarket.vercel.app/`}
+                      target="_blank"
+                    >
+                      mohosseinimarket.vercel.app
+                    </Link>
+                  </Button>
+                </div>
               </div>
               <div className="mt-2 pl-2 flex flex-col gap-1">
-                <span>number one</span>
-                <span>number one</span>
-                <span>number one</span>
-                <span>number one</span>
+                <p className="text-muted-foreground text-sm">
+                  Developed a full-featured e-commerce platform with Next.js,
+                  including performance optimization (Lighthouse 90+ and bundle
+                  reduction via RSC), secure authentication (next-auth) with
+                  route protection using a proxy, multi-layer filtering and
+                  search with shareable URL parameters and pagination, and a
+                  persistent shopping cart powered by Zustand with a clean UI
+                  for instant feedback and reduced latency.
+                </p>
               </div>
             </li>
           </ul>
