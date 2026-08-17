3ac5f8b3eff176343345bbd530b1c2cb848e61df
kasra <kasraam385@gmail.com>
2026-08-17 18:32:14 +0330

    make a template noice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,9 +16,27 @@ export default function Home() {
           </h1>
         </div>
         <div className="flex flex-col">
-          <h1 className="text-lg sm:text-xl lg:text-2xl mb-4 font-semibold">
+          <h1 className="text-lg sm:text-xl lg:text-2xl mb-4 font-extrabold">
             Work EXP
           </h1>
+          <ul className="border-l flex flex-col gap-4 relative py-4">
+            <li className="flex flex-col">
+              <div className="flex items-center">
+                <span className="bg-primary font-bold text-secondary relative -left-5 w-10 h-10 flex items-center justify-center">
+                  1
+                </span>
+                <span className="text-xl font-semibold">
+                  Online super market
+                </span>
+              </div>
+              <div className="mt-2 pl-2 flex flex-col gap-1">
+                <span>number one</span>
+                <span>number one</span>
+                <span>number one</span>
+                <span>number one</span>
+              </div>
+            </li>
+          </ul>
         </div>
       </section>
     </div>
--- src/consts/navLinks.ts
+++ src/consts/navLinks.ts
@@ -1,7 +1,7 @@
-import { CircleUserRoundIcon, FolderOpenDotIcon, HomeIcon } from "lucide-react";
+import { BookHeartIcon, CircleUserRoundIcon, HomeIcon } from "lucide-react";
 
 export const navLinks = [
   { label: "Sweet home", href: "/", icon: HomeIcon },
-  { label: "Guest book", href: "/guest-book", icon: FolderOpenDotIcon },
+  { label: "Guest book", href: "/guest-book", icon: BookHeartIcon },
   { label: "About me", href: "/about-me", icon: CircleUserRoundIcon },
 ];
