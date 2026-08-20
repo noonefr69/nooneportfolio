5497340743d75e0469a6635e0b0b22ad3fe675c9
kasra <kasraam385@gmail.com>
2026-08-20 19:07:22 +0330

    clean the guest book page and add form file to it

--- src/app/(pages)/guest-book/page.tsx
+++ src/app/(pages)/guest-book/page.tsx
@@ -1,72 +1,7 @@
-"use client";
-
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Controller, useForm } from "react-hook-form";
-import { toast } from "sonner";
-import * as z from "zod";
-
-import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { useTransition } from "react";
-import { createGuestbook } from "@/app/actions/create-guestbook";
-import { ArrowUpToLine, ChevronRight } from "lucide-react";
-import { Spinner } from "@/components/ui/spinner";
-
-// 1. Create the form schema
-const formSchema = z.object({
-  username: z.string().min(1, "is required.").max(8, "Max 8 characters."),
-  message: z
-    .string()
-    .min(1, "Message cannot be empty.")
-    .max(200, "Max 200 characters."),
-  human: z
-    .boolean()
-    .refine((val) => val === true, "Please verify you are human."),
-});
+import GuestbookForm from "@/components/guestbook/form";
 
 export default function GuestBook() {
-  const [isPending, startTransition] = useTransition();
-
-  // 2. Initialize the form
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-      message: "",
-      human: false,
-    },
-  });
-
-  // 3. Dummy submit function (UI only)
-  function onSubmit(data: z.infer<typeof formSchema>) {
-    startTransition(async () => {
-      try {
-        const action = await createGuestbook(data);
-
-        if (!action.success) {
-          toast.error(action.error);
-          return;
-        }
-
-        toast.success(action.message);
-        form.reset();
-      } catch (err) {
-        console.error(err);
-        toast.error(`something went wrong`);
-      }
-    });
-    // toast("Guestbook submission:", {
-    //   description: (
-    //     <pre className="mt-2 w-[320px] overflow-x-auto rounded-md bg-code p-4 text-code-foreground">
-    //       <code>{JSON.stringify(data, null, 2)}</code>
-    //     </pre>
-    //   ),
-    //   position: "bottom-right",
-    // });
-    // form.reset();
-  }
-
   return (
     <div className="mt-20">
       <section className="flex-1 min-h-0 h-[80vh] lg:h-[60vh] grid grid-cols-9 gap-2">
@@ -89,7 +24,6 @@ export default function GuestBook() {
                 </span>
               </pre>
             </div>
-
             {/* Messages Area */}
             <div className="flex-1 overflow-y-auto space-y-3 text-sm pr-2">
               {/* Dummy Message 1 */}
@@ -101,107 +35,7 @@ export default function GuestBook() {
                 </span>
               </div>
             </div>
-
-            {/* Input Area - Wrapped in Form */}
-            <form
-              onSubmit={form.handleSubmit(onSubmit)}
-              className="mt-4 pt-4 border-t border-border space-y-3"
-            >
-              {/* Row 1: Username & Bot Check */}
-              <div className="flex items-center gap-2">
-                <div className="flex flex-col w-full max-w-30">
-                  <Controller
-                    name="username"
-                    control={form.control}
-                    render={({ field, fieldState }) => (
-                      <>
-                        <Input
-                          {...field}
-                          type="text"
-                          placeholder="username"
-                          className={`h-8 text-sm focus-visible:ring-0 ${fieldState.invalid ? "border-destructive" : ""}`}
-                          aria-invalid={fieldState.invalid}
-                        />
-                        {/*{fieldState.invalid && (
-                          <span className="text-xs text-destructive mt-1 absolute bottom-0 left-32 ">
-                            {fieldState.error?.message}
-                          </span>
-                        )}*/}
-                      </>
-                    )}
-                  />
-                </div>
-
-                <div className={`flex flex-col items-end`}>
-                  <Controller
-                    name="human"
-                    control={form.control}
-                    render={({ field, fieldState }) => (
-                      <Card
-                        className={`p-2 ${fieldState.invalid ? "border-destructive ring-destructive outline-destructive" : ""}`}
-                      >
-                        <label className="flex items-center gap-2 text-xs text-muted-foreground cursor-pointer select-none">
-                          <Input
-                            type="checkbox"
-                            checked={field.value}
-                            onChange={field.onChange}
-                            className={`accent-primary h-3 w-3 cursor-pointer ${fieldState.invalid ? "border-destructive" : ""}`}
-                            aria-invalid={fieldState.invalid}
-                          />
-                          human?
-                        </label>
-                      </Card>
-                    )}
-                  />
-                  {/*{form.formState.errors.human && (
-                    <span className="text-xs text-destructive mt-1">
-                      {form.formState.errors.human.message}
-                    </span>
-                  )}*/}
-                </div>
-              </div>
-
-              {/* Row 2: Message & Send */}
-              <div className="flex flex-col gap-2 pb-3 relative">
-                <Controller
-                  name="message"
-                  control={form.control}
-                  render={({ field, fieldState }) => (
-                    <>
-                      <div className="flex gap-2">
-                        <Input
-                          {...field}
-                          type="text"
-                          placeholder="type a message..."
-                          className={`h-8 flex-1 text-sm focus-visible:ring-0 ${fieldState.invalid ? "border-destructive" : ""}`}
-                          aria-invalid={fieldState.invalid}
-                        />
-                        <Button
-                          type="submit"
-                          variant="outline"
-                          size="sm"
-                          className="h-8 w-24 text-sm group"
-                        >
-                          {isPending ? (
-                            <Spinner />
-                          ) : (
-                            <>
-                              submit
-                              <ChevronRight className="duration-100 group-hover:-rotate-12" />
-                            </>
-                          )}{" "}
-                        </Button>
-                      </div>
-                      {/*{fieldState.invalid && (
-                        <span className="text-xs text-destructive absolute right-24 bottom-5">
-                          {fieldState.error?.message}
-                        </span>
-                      )}*/}
-                    </>
-                  )}
-                />
-              </div>
-            </form>
+            <GuestbookForm />
           </CardContent>
         </Card>
       </section>
---
+++ src/components/guestbook/form.tsx
@@ -0,0 +1,160 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import { toast } from "sonner";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { useTransition } from "react";
+import { createGuestbook } from "@/app/actions/create-guestbook";
+import { ChevronRight } from "lucide-react";
+import { Spinner } from "@/components/ui/spinner";
+
+const formSchema = z.object({
+  username: z.string().min(1, "is required.").max(8, "Max 8 characters."),
+  message: z
+    .string()
+    .min(1, "Message cannot be empty.")
+    .max(200, "Max 200 characters."),
+  human: z
+    .boolean()
+    .refine((val) => val === true, "Please verify you are human."),
+});
+
+export default function GuestbookForm() {
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      message: "",
+      human: false,
+    },
+  });
+
+  // 3. Dummy submit function (UI only)
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    startTransition(async () => {
+      try {
+        const action = await createGuestbook(data);
+
+        if (!action.success) {
+          toast.error(action.error);
+          return;
+        }
+
+        toast.success(action.message);
+        form.reset();
+      } catch (err) {
+        console.error(err);
+        toast.error(`something went wrong`);
+      }
+    });
+  }
+
+  return (
+    <form
+      onSubmit={form.handleSubmit(onSubmit)}
+      className="mt-4 pt-4 border-t border-border space-y-3"
+    >
+      {/* Row 1: Username & Bot Check */}
+      <div className="flex items-center gap-2">
+        <div className="flex flex-col w-full max-w-30">
+          <Controller
+            name="username"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <>
+                <Input
+                  {...field}
+                  type="text"
+                  placeholder="username"
+                  className={`h-8 text-sm focus-visible:ring-0 ${fieldState.invalid ? "border-destructive" : ""}`}
+                  aria-invalid={fieldState.invalid}
+                />
+                {/*{fieldState.invalid && (
+                           <span className="text-xs text-destructive mt-1 absolute bottom-0 left-32 ">
+                             {fieldState.error?.message}
+                           </span>
+                         )}*/}
+              </>
+            )}
+          />
+        </div>
+
+        <div className={`flex flex-col items-end`}>
+          <Controller
+            name="human"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Card
+                className={`p-2 ${fieldState.invalid ? "border-destructive ring-destructive outline-destructive" : ""}`}
+              >
+                <label className="flex items-center gap-2 text-xs text-muted-foreground cursor-pointer select-none">
+                  <Input
+                    type="checkbox"
+                    checked={field.value}
+                    onChange={field.onChange}
+                    className={`accent-primary h-3 w-3 cursor-pointer ${fieldState.invalid ? "border-destructive" : ""}`}
+                    aria-invalid={fieldState.invalid}
+                  />
+                  human?
+                </label>
+              </Card>
+            )}
+          />
+          {/*{form.formState.errors.human && (
+                     <span className="text-xs text-destructive mt-1">
+                       {form.formState.errors.human.message}
+                     </span>
+                   )}*/}
+        </div>
+      </div>
+
+      {/* Row 2: Message & Send */}
+      <div className="flex flex-col gap-2 pb-3 relative">
+        <Controller
+          name="message"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <>
+              <div className="flex gap-2">
+                <Input
+                  {...field}
+                  type="text"
+                  placeholder="type a message..."
+                  className={`h-8 flex-1 text-sm focus-visible:ring-0 ${fieldState.invalid ? "border-destructive" : ""}`}
+                  aria-invalid={fieldState.invalid}
+                />
+                <Button
+                  type="submit"
+                  variant="outline"
+                  size="sm"
+                  className="h-8 w-24 text-sm group"
+                >
+                  {isPending ? (
+                    <Spinner />
+                  ) : (
+                    <>
+                      submit
+                      <ChevronRight className="duration-100 group-hover:-rotate-12" />
+                    </>
+                  )}{" "}
+                </Button>
+              </div>
+              {/*{fieldState.invalid && (
+                         <span className="text-xs text-destructive absolute right-24 bottom-5">
+                           {fieldState.error?.message}
+                         </span>
+                       )}*/}
+            </>
+          )}
+        />
+      </div>
+    </form>
+  );
+}
